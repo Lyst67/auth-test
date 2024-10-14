4b5ea123537d90494903fbea079b7780c940ecb5
pavlo lystopad <p_listopad@ukr.net>
2024-10-14 16:52:33 +0300

    upd_button

--- src/app/components/telegramSignForm.tsx
+++ src/app/components/telegramSignForm.tsx
@@ -2,7 +2,7 @@ import { buttonStyle } from "../lib/utils/styleTemplates";
 // import { getSocialLogIn } from "@/actions/login";
 import { BiLogoTelegram } from "react-icons/bi";
 import { LoginButton } from "@telegram-auth/react";
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
 
 export default function TelegramAccountButton({
   botUsername,
@@ -16,20 +16,28 @@ export default function TelegramAccountButton({
     >
       <button
         type="submit"
-        name="action"
-        value="google"
+        // name="action"
+        // value="google"
         className={`${buttonStyle} bg-transparent text-main-text px-3 hover:bg-faceblue dark:text-light-text`}
       >
         Log In with Telegram
         <div className="w-9 h-9 rounded-full bg-faceblue grid place-items-center">
           <BiLogoTelegram className="w-7 h-7" />
         </div>
-        <LoginButton
+        {/* <LoginButton
           botUsername={botUsername}
           onAuthCallback={(data) => {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             signIn("telegram-login", { callbackUrl: "/user" }, data as any);
           }}
+        /> */}
+        <LoginButton
+          botUsername={botUsername}
+          authCallbackUrl="https://telegram-auth-rho.vercel.app/"
+          buttonSize="large" // "large" | "medium" | "small"
+          cornerRadius={5} // 0 - 20
+          showAvatar={true} // true | false
+          lang="en"
         />
       </button>
     </form>
