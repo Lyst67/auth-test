cdf2f7a51d2afbb9f7f7d48b43fe0de402e346ed
pavlo lystopad <p_listopad@ukr.net>
2024-10-16 17:14:10 +0300

    fix_user_info

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../lib/auth";
-import { prisma } from "../lib/prisma";
+// import { prisma } from "../lib/prisma";
 
 export default async function UserPage() {
 	const session = await getServerSession(authOptions);
@@ -12,19 +12,19 @@ export default async function UserPage() {
 			</div>
 		);
 	}
-
-	const user = await prisma.user.findUnique({
-		where: {
-			id: session.user.id,
-		},
-	});
+	const user = session?.user;
+	// const user = await prisma.user.findUnique({
+	// 	where: {
+	// 		id: session?.user.id,
+	// 	},
+	// });
 
 	return (
 		<div className="flex flex-col items-center mt-10">
 			<h1>{user?.name}</h1>
 			<h1 className="truncate max-w-[300px]">{user?.image}</h1>
 			<h1>{user?.id}</h1>
-			<h1>{user?.createdAt.getTime()}</h1>
+			{/* <h1>{user?.createdAt.getTime()}</h1> */}
 		</div>
 	);
 }
