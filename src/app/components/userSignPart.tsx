fe115cfe5a404389c2b63c45e5f0b134f862b935
pavlo lystopad <p_listopad@ukr.net>
2024-10-16 15:06:36 +0300

    fix_user_type

--- src/app/components/userSignPart.tsx
+++ src/app/components/userSignPart.tsx
@@ -1,46 +1,47 @@
 import Image from "next/image";
 import LogOut from "./logOut";
 import SignButton from "./signButton";
-import { User } from "../lib/auth";
 import Link from "next/link";
 
 type Props = {
-  user: User;
+	user:
+		| { id: string; name: string; image: string; email: string }
+		| undefined;
 };
 
 export default function UserSignPart({ user }: Props) {
-  const userName: string | null | undefined = user?.name;
-  const userImage: string | null | undefined = user?.image;
-  return (
-    <>
-      {!user ? (
-        <div className="flex gap-1 lg:flex-col xl:flex-row xl:gap-2">
-          <Link href={"?login=true"}>
-            <SignButton type="button">Log In</SignButton>
-          </Link>
-        </div>
-      ) : (
-        <div className="flex flex-col gap-2">
-          <p className="ml-auto">{userName}</p>
-          <div className="flex gap-2">
-            <div className="w-8 h-8 ml-5 rounded-full overflow-hidden">
-              {userName && userImage && (
-                <Image
-                  className="w-8 h-8"
-                  src={userImage}
-                  alt={userName}
-                  width="0"
-                  height="0"
-                  sizes="100vw"
-                />
-              )}
-            </div>
-            <div className="w-fit ml-auto">
-              <LogOut />
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+	const userName: string | null | undefined = user?.name;
+	const userImage: string | null | undefined = user?.image;
+	return (
+		<>
+			{!user ? (
+				<div className="flex gap-1 lg:flex-col xl:flex-row xl:gap-2">
+					<Link href={"?login=true"}>
+						<SignButton type="button">Log In</SignButton>
+					</Link>
+				</div>
+			) : (
+				<div className="flex flex-col gap-2">
+					<p className="ml-auto">{userName}</p>
+					<div className="flex gap-2">
+						<div className="w-8 h-8 ml-5 rounded-full overflow-hidden">
+							{userName && userImage && (
+								<Image
+									className="w-8 h-8"
+									src={userImage}
+									alt={userName}
+									width="0"
+									height="0"
+									sizes="100vw"
+								/>
+							)}
+						</div>
+						<div className="w-fit ml-auto">
+							<LogOut />
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
