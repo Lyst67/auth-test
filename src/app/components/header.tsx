496ab7619c4f2f623b41c2d7a89e6e172fc16f2d
pavlo lystopad <p_listopad@ukr.net>
2024-10-16 14:44:32 +0300

    fix_header_user-type

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,39 +4,38 @@ import { useSession } from "next-auth/react";
 import { navPages } from "../lib/navPages";
 import NavLinks from "./navLinks";
 import UserSignPart from "./userSignPart";
-import { User } from "../lib/auth";
 import LogInModal from "./logInModal";
 
 export default function Header() {
-  const { data: session, status } = useSession();
+	const { data: session, status } = useSession();
 
-  if (status === "loading") {
-    return (
-      <div>
-        <h2>Loading...</h2>
-      </div>
-    );
-  }
+	if (status === "loading") {
+		return (
+			<div>
+				<h2>Loading...</h2>
+			</div>
+		);
+	}
 
-  const user = session?.user as User;
+	const user = session?.user;
 
-  return (
-    <>
-      <div className="h-20 px-5 py-3 flex items-center justify-between bg-dark-bg text-light-text">
-        {/* <LogoItem /> */}
-        <div className="mx-auto">
-          <NavLinks
-            navPages={navPages}
-            // admin={admin}
-          />
-        </div>
+	return (
+		<>
+			<div className="h-20 px-5 py-3 flex items-center justify-between bg-dark-bg text-light-text">
+				{/* <LogoItem /> */}
+				<div className="mx-auto">
+					<NavLinks
+						navPages={navPages}
+						// admin={admin}
+					/>
+				</div>
 
-        <div className="flex gap-4 items-center">
-          <UserSignPart user={user} />
-        </div>
-        <LogInModal botUsername={`${process.env.BOT_USERNAME}`} />
-      </div>
-      <hr className=" border-solid border-transparent dark:border-slate-600" />
-    </>
-  );
+				<div className="flex gap-4 items-center">
+					<UserSignPart user={user} />
+				</div>
+				<LogInModal botUsername={`${process.env.BOT_USERNAME}`} />
+			</div>
+			<hr className=" border-solid border-transparent dark:border-slate-600" />
+		</>
+	);
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,18 +1,30 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../lib/auth";
+import { prisma } from "../lib/prisma";
 
 export default async function UserPage() {
-  const session = await getServerSession(authOptions);
-  console.log(session?.user);
-  if (!session) {
-    return <h1>Not logged in to see what is here</h1>;
-  }
+	const session = await getServerSession(authOptions);
 
-  return (
-    <div>
-      <h1>You can see this because you are logged in.</h1>
+	if (!session) {
+		return (
+			<div className="flex justify-center mt-10">
+				Not logged in to see this
+			</div>
+		);
+	}
 
-      <pre>{JSON.stringify(session.user, null, 2)}</pre>
-    </div>
-  );
+	const user = await prisma.user.findUnique({
+		where: {
+			id: session.user.id,
+		},
+	});
+
+	return (
+		<div className="flex flex-col items-center mt-10">
+			<h1>{user?.name}</h1>
+			<h1 className="truncate max-w-[300px]">{user?.image}</h1>
+			<h1>{user?.id}</h1>
+			<h1>{user?.createdAt.getTime()}</h1>
+		</div>
+	);
 }
